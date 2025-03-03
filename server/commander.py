c448f29c89ceb54d2e2afb3ca9beec417ff7d354
ruiange <bombingpig@outlook.com>
2025-03-03 19:14:33 +0800

    feat(server): 优化视频下载和发送功能
    - 更新视频下载链接和流程
    - 添加时间延迟以避免频繁请求
    - 修正文件发送函数参数顺序
    - 新增 send_video.py 文件用于发送视频消息

--- server/commander.py
+++ server/commander.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 from server.send_file import send_file_message
 from utils.download import download
@@ -8,7 +9,8 @@ def bot_commander(wcf, msg):
     logging.info("命令监听已启动，等待新命令...")
     if "douyin" in msg.content:
         logging.info(msg.content)
-        file_path = download('https://9yin.woniu.com/static/web201588/video/201908.mp4')
+        file_path = download('http://101.126.92.189:8090/api/download?url=https://v.douyin.com/i5kLmruj/&prefix=true&with_watermark=true')
         logging.info("下载完成: %s", file_path)
         # 等待1秒执行
+        time.sleep(1)
         send_file_message(wcf, msg.roomid, file_path)
--- server/send_video.py
+++ server/send_video.py
@@ -1,5 +1,6 @@
 import logging
 
+
 def send_file_message(wcf, recipient, path, callback=None):
     logging.info(path)
     """
@@ -11,26 +12,25 @@ def send_file_message(wcf, recipient, path, callback=None):
     :return: (bool, str) 元组，表示 (是否成功, 错误信息)
     """
     try:
-        # 发送文件消息
-        # 假设 wcf 有一个 send_file 方法
-        wcf.send_file(path,recipient)
+        # 发送视频消息
+        wcf.send_video(path, recipient)
         success = True
         error_msg = ""
-        
+
         # 如果有回调函数，调用它
         if callback:
             callback(success, error_msg)
-        
+
         return success, error_msg
 
     except Exception as e:
         logging.error(f"发送文件失败: {str(e)}")
         success = False
         error_msg = str(e)
-        
+
         # 如果有回调函数，调用它
         if callback:
             callback(success, error_msg)
-        
+
         return success, error_msg
 
